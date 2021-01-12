de36d450ed1abf73856edbcd095deccdf7dadee4
Zhouchaozhi <18726021577@gmail.com>
2021-01-12 14:23:20 +0800

    修改UI适配

---
+++ UnityARFace/UnityARFace/UnityARFaceDemo/Assets/Scripts/SubmarineBattle/CameraMgr.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMgr : MonoBehaviour
+{
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    Debug.Log(Screen.width + " ," + Screen.height);
+    //    Camera.main.orthographicSize = Screen.height / 100 / 2;
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    public float initOrthoSize;
+    public float initWidth;
+    public float initHeight;
+
+    float factWidth;
+    float factHeight;
+
+    void Start()
+    {
+        factWidth = Screen.width;
+        factHeight = Screen.height;
+        //实际正交视口 = 初始正交视口 * 初始宽高比 / 实际宽高比
+        GetComponent<Camera>().orthographicSize = (initOrthoSize * (initWidth / initHeight)) / (factWidth / factHeight);
+    }
+}
